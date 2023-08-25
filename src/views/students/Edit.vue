86e2361336cd378c40a874c2d0360c27f18c4b2b
Md. Himel Ali <himel.phy160@gmail.com>
2023-08-25 14:41:04 +0600

    Update page Added

--- src/views/students/Edit.vue
+++ src/views/students/Edit.vue
@@ -2,6 +2,7 @@
 import axios from "axios"
 import {onMounted, ref} from "vue"
 import {useRoute} from "vue-router"
+import router from "@/router";
 const student = ref('')
 const error = ref('')
 async function getSingleStudent(id){
@@ -19,8 +20,13 @@ onMounted(()=>{
   getSingleStudent(route.params.id)
 })
 
-function update(){
 
+async function update(id, data){
+  const url = 'http://localhost:3000/studentList/'
+ const response = await axios.put(url + id, data )
+  if (response.status === 200){
+    await router.push('/student')
+  }
 }
 </script>
 
@@ -28,50 +34,49 @@ function update(){
   <section class="container mx-auto flex flex-col items-center justify-center font-nunito">
     <div class="max-w-3xl w-full py-10 bg-white my-16 shadow shadow-blue-100 px-10">
       <h1 class="pb-5 text-center text-2xl font-semibold ">Edit Student Data</h1>
-
       <div class="w-full">
         <div class="flex pb-5 gap-4 items-center justify-between">
           <div class="w-1/2">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Student Name <span class="text-red-500">*</span></label>
-            <input type="email" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <input v-model="student.name"  type="text" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
           </div>
           <div class="w-1/2">
             <label for="roll" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Roll <span class="text-red-500">*</span></label>
-            <input type="text" id="roll" class="bg-white border focus:outline-blue-300 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <input v-model="student.roll" type="text" id="roll" class="bg-white border focus:outline-blue-300 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           </div>
         </div>
         <div class="flex pb-5 gap-4 items-center justify-between">
           <div class="w-1/2">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Class <span class="text-red-500">*</span></label>
-            <input type="email" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <input v-model="student.class" type="text" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
           </div>
           <div class="w-1/2">
             <label for="roll" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender  <span class="text-red-500">*</span></label>
-            <input type="text" id="roll" class="bg-white focus:outline-blue-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <input v-model="student.gender" type="text" id="roll" class="bg-white focus:outline-blue-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           </div>
         </div>
         <div class="flex pb-5 gap-4 items-center justify-between">
           <div class="w-1/2">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Blood Group <span class="text-red-500">*</span></label>
-            <input type="email" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <input v-model="student.bloodGroup" type="email" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
           </div>
           <div class="w-1/2">
             <label for="roll" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Religion  <span class="text-red-500">*</span></label>
-            <input type="text" id="roll" class="bg-white focus:outline-blue-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <input v-model="student.religion" type="text" id="roll" class="bg-white focus:outline-blue-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           </div>
         </div>
         <div class="flex pb-5 gap-4 items-center justify-between">
           <div class="w-1/2">
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone <span class="text-red-500">*</span></label>
-            <input type="email" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <input v-model="student.phone" type="email" id="name" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-blue-300 focus:ring-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
           </div>
           <div class="w-1/2">
             <label for="roll" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address  <span class="text-red-500">*</span></label>
-            <input type="text" id="roll" class="bg-white focus:outline-blue-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <input v-model="student.address" type="text" id="roll" class="bg-white focus:outline-blue-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
           </div>
         </div>
         <div class="flex gap-5 items-center justify-center">
-          <button type="button" @click="update()" class="bg-blue-500 px-16 py-2 rounded-full text-white text-center">Update</button>
+          <button type="button" @click="update(route.params.id, student)" class="bg-blue-500 px-16 py-2 rounded-full text-white text-center">Update</button>
         </div>
       </div>
 
